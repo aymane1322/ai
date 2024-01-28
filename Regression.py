c8d67ffb6cb3e3a9700a80ce5c579694ffa3746c
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-01-28 20:26:42 +0100

    a

--- Regression.py
+++ Regression.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error 
 
 data = pd.read_csv("market.csv")
 
